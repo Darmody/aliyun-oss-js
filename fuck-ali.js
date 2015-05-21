dcc34617da79bdd9e885493632c0fe0c91aaeef1
darmody <darmody@che3bao.com>
2015-05-21 18:47:19 +0800

    完成0.0.1 alpha

---
+++ fuck-ali.js
@@ -0,0 +1,55 @@
+(function(factory) {
+
+  var root = window;
+
+  root.fa = factory(root, {}, root.ss);
+
+}(function(root, fa, ss) {
+
+  fa.VERSION = "0.0.1";
+
+  var configuration = {
+    filename: "file"
+  };
+
+  var conf = function(config) {
+
+    configuration.button = config.button || "";
+    configuration.url = config.url || "";
+    configuration.policy = config.policy || "";
+    configuration.signature = config.signature || "";
+    configuration.filename = config.filename || "";
+    configuration.osskey = config.osskey || "";
+    configuration.onComplete = config.onComplete || "";
+    configuration.onProgress = config.onProgress || "";
+    configuration.onError = config.onError || "";
+  }
+
+  fa.upload = function(config) {
+
+    conf(config);
+
+    var uploader = new ss.SimpleUpload({
+      button: document.getElementById(configuration.button),
+      url: configuration.url,
+      name: "file",
+      cors: true,
+      multipart: true,
+      method: "post",
+      data: {
+        OSSAccessKeyId: configuration.osskey,
+        policy: configuration.policy,
+        Signature: configuration.signature,
+        success_action_status: "201",
+        key: configuration.filename
+      },
+      onComplete: configuration.onComplete,
+      onProgress: configuration.onProgress,
+      onError: configuration.onError
+    });
+
+    return uploader;
+  }
+
+  return fa;
+}));
---
+++ lib/test/run.rb
@@ -0,0 +1,3 @@
+100.times do
+  system("ruby sign.rb")
+end
